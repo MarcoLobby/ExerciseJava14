cd487bac8b1a377ccbbf4d009064f0493f65fd57
Marco Lo Bello <xSalamander@outlook.it>
2022-12-12 17:31:39 +0100

    Initial Commit

---
+++ src/it/DevelHope/Exercise14/Start.java
@@ -0,0 +1,10 @@
+package it.DevelHope.Exercise14;
+
+public class Start {
+
+    public static void main(String[] args) {
+
+        String mySurname = "Lo Bello";
+        System.out.println(mySurname.contains("ni") ? "Your surname contains the sequence of letters 'ni'" : "Your surname doesn't contain the sequence of letters 'ni'");
+    }
+}
